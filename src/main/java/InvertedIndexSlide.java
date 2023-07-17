23d65e79e3aa7c358c5534d0ebeca857d9aa78a0
Erik Hatcher <erik.hatcher@gmail.com>
2023-07-17 16:10:51 -0600

    Render postings list in columns instead of left-justified

--- src/main/java/InvertedIndexSlide.java
+++ src/main/java/InvertedIndexSlide.java
@@ -13,6 +13,7 @@ import org.apache.lucene.index.Terms;
 import org.apache.lucene.index.TermsEnum;
 import org.apache.lucene.search.DocIdSetIterator;
 import org.apache.lucene.store.ByteBuffersDirectory;
+import processing.core.PImage;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -22,6 +23,8 @@ public class InvertedIndexSlide extends BaseSlide {
   final List<Document> docs = new ArrayList<>();
 
   final LuceneAnalyzer text_analyzer = new LuceneAnalyzer();
+  private final int MAX_DOCS = 10;
+  private final PImage[] doc_images = new PImage[MAX_DOCS];
 
   public InvertedIndexSlide(String title, ProcessCene presentation) {
     super(title, presentation);
@@ -32,20 +35,30 @@ public class InvertedIndexSlide extends BaseSlide {
     // General_ACTION_Favorite_Inverted10x.png
     // Technical_MDB_DocumentModel10x.png
 
-    Document doc1 = new Document();
-    doc1.add(new StringField("id", "0", Field.Store.YES));
-    doc1.add(new TextField("title", "Uberconf 2023: Java and much more", Field.Store.YES));
-    docs.add(doc1);
+    Document doc = new Document();
+    doc.add(new StringField("id", "1", Field.Store.YES));
+    doc.add(new TextField("title", "Uberconf 2023: Java and much more", Field.Store.YES));
+    docs.add(doc);
 
-    Document doc2 = new Document();
-    doc2.add(new StringField("id", "1", Field.Store.YES));
-    doc2.add(new TextField("title", "Much Java was needed this morning", Field.Store.YES));
-    docs.add(doc2);
+    doc = new Document();
+    doc.add(new StringField("id", "2", Field.Store.YES));
+    doc.add(new TextField("title", "Much Java was needed this morning", Field.Store.YES));
+    docs.add(doc);
+
+    doc = new Document();
+    doc.add(new StringField("id", "3", Field.Store.YES));
+    doc.add(new TextField("title", "Blake Loves Java!!!", Field.Store.YES));
+    docs.add(doc);
+
+    for (int i=0; i < 10; i++) {
+      doc_images[i] = presentation.loadImage(presentation.getFilePathFromResources("Assets/normal/" + i + "_Inverted10x.png"));
+      doc_images[i].resize(30,30);
+    }
   }
 
   @Override
   public void draw(int step) {
-    presentation.textFont(presentation.loadFont(presentation.getFilePathFromResources("SourceCodeProRoman-Medium-24.vlw")));
+    //presentation.textFont(presentation.loadFont(presentation.getFilePathFromResources("SourceCodeProRoman-Medium-24.vlw")));
 
     ByteBuffersDirectory index = new ByteBuffersDirectory();
     // TODO: Make analyzer name cycle
@@ -66,31 +79,63 @@ public class InvertedIndexSlide extends BaseSlide {
 
     for (int i = 0; i < step; i++) {
       Document doc = docs.get(i);
-      String doc_string = doc.get("id") + ": " + doc.get("title");
-      presentation.text(doc_string,x,y);
+      PImage doc_image = doc_images[Integer.parseInt(doc.get("id"))];
+      presentation.image(doc_image, x, y - presentation.textAscent() - presentation.textDescent());
+      presentation.text(doc.get("title"),x + doc_image.width,y);
       y += 40;
     }
 
     x = 0;
     y = 200;
+    float top_of_postings_y = y - presentation.textAscent();
+    /*
+          Other index stats: avg field length?
+
+          term | df |      postings list        |
+          foo  |  3 |  "
+
+     */
     try {
       IndexReader reader = DirectoryReader.open(index);
       Terms terms = MultiTerms.getTerms(reader,"title");
+
+      float df_x = x + presentation.textWidth("XXXXXXXXXXXXX");
+      float posting_list_x = df_x + presentation.textWidth("XXX");
+
       if (terms != null) {
+        presentation.text("Term", x, y);
+        presentation.text("df", df_x, y);
+        String postings_list_header = "Postings List";
+        presentation.text(postings_list_header, posting_list_x, y);
+
+        // draw line under posting list header row
+        presentation.line(x,y + presentation.textDescent(),posting_list_x + presentation.textWidth(postings_list_header),y + presentation.textDescent());
+
+        y += presentation.textAscent() + presentation.textDescent() + 15;
+
         TermsEnum terms_enum = terms.iterator();
         while (terms_enum.next() != null) {
           int df = terms_enum.docFreq();
-          String txt = terms_enum.term().utf8ToString();
+          String term_value = terms_enum.term().utf8ToString();
 
-          String postings_list = "Postings: ";
+          presentation.text(term_value, x, y);
+          presentation.text(df, df_x, y);
+
           PostingsEnum postings = terms_enum.postings(null);
           while (postings.nextDoc() != DocIdSetIterator.NO_MORE_DOCS) {
-            postings_list += postings.docID() + " - ";
+            int id = Integer.parseInt(reader.storedFields().document(postings.docID()).get("id"));
+            PImage doc_image = doc_images[id];
+            presentation.image(doc_image, posting_list_x + doc_image.width * (id - 1), y - presentation.textAscent() - presentation.textDescent());
           }
-          presentation.text("Term: " + txt + " (" + df + ") :: " + postings_list, x, y);
+          // horizontal line under posting
+          presentation.line(x,y + presentation.textDescent(),posting_list_x + presentation.textWidth(postings_list_header),y + presentation.textDescent());
 
-          y += presentation.textAscent() + presentation.textDescent() + 10;
+          y += presentation.textAscent() + presentation.textDescent() + 15;
         }
+
+        // draw lines for the columns
+        presentation.line(df_x - 5,top_of_postings_y, df_x - 5, y - presentation.textAscent() - presentation.textDescent());
+        presentation.line(posting_list_x - 5,top_of_postings_y,posting_list_x - 5,y - presentation.textAscent() - presentation.textDescent());
       }
     } catch (IOException e) {
       throw new RuntimeException(e);
