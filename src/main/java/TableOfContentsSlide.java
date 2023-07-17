97480cfa32c750de43fcee729d098dade34c9219
Erik Hatcher <erik.hatcher@gmail.com>
2023-07-17 13:38:31 -0600

    ToC slide can be created to highlight at a particular item

--- src/main/java/TableOfContentsSlide.java
+++ src/main/java/TableOfContentsSlide.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class TableOfContentsSlide extends BaseSlide {
   private final List<Slide> slides;
+  private int current_slide_index = -1; // main ToC, don't highlight any
 
   PImage bullet_image;
 
@@ -14,10 +15,16 @@ public class TableOfContentsSlide extends BaseSlide {
   }
 
   public TableOfContentsSlide(List<Slide> slides, PImage bullet_image, ProcessCene presentation) {
-    super("Table Of Contents", presentation);
+    super("Table of Contents", presentation);
 
     this.slides = slides;
     this.bullet_image = bullet_image;
+  }
+
+  public TableOfContentsSlide(List<Slide> slides, PImage bullet_image, int current_slide_index, ProcessCene presentation) {
+    this(slides, bullet_image, presentation);
+    this.current_slide_index = current_slide_index;
+    title = "Table Of Contents" + ((current_slide_index > -1) ? ": " + slides.get(current_slide_index).getTitle(): "");
   }
 
   @Override
@@ -40,7 +47,7 @@ public class TableOfContentsSlide extends BaseSlide {
         boolean me = false;
         if (title != null) {
           presentation.image(bullet_image, x, y);
-          presentation.text(title, x + bullet_image.width, y + bullet_image.height - presentation.textAscent());
+          presentation.text(((current_slide_index == i)?"* ":"") + title, x + bullet_image.width, y + bullet_image.height - presentation.textAscent());
 
           // TODO: highlight slide title text that is being hovered on
 
