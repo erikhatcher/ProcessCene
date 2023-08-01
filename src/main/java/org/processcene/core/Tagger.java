df1e8ae7f6490b5f63da43306f517d0904e3eb34
Erik Hatcher <erik.hatcher@gmail.com>
2023-08-01 12:22:59 -0400

    comment on tagger response format

--- src/main/java/org/processcene/core/Tagger.java
+++ src/main/java/org/processcene/core/Tagger.java
@@ -3,5 +3,11 @@ package org.processcene.core;
 import org.json.simple.JSONObject;
 
 public interface Tagger {
+  /**
+   * For now this returns a Solr JSON response
+   *
+   * @param input text to be tagged
+   * @return
+   */
   JSONObject tag(String input);
 }
