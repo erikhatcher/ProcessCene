291cb07f2461adbe88b05c36b9007d71623253b9
Erik Hatcher <erik.hatcher@gmail.com>
2023-08-22 19:05:54 -0400

    add handy print() method for diagnostic output of SearchResponse

--- src/main/java/org/processcene/core/SearchResponse.java
+++ src/main/java/org/processcene/core/SearchResponse.java
@@ -10,6 +10,22 @@ public class SearchResponse {
   public float max_score = 0.0f;
 
   public List<ResponseDocument> documents = new ArrayList<>();
+  public String explain; // TODO: Implement?  For Atlas it's the explain(), for Solr it'd be the full debug node output, for Lucene (??)
+  public String parsed_query;
+  public long query_time = -1;
+
+  public void print() {
+    System.out.println("SearchResponse: ");
+    System.out.println("  error: " + error);
+    System.out.println("  parsed_query: " + parsed_query);
+    System.out.println("  max_score: " + max_score);
+    System.out.println("  explain: " + explain);
+
+    for (ResponseDocument document : documents) {
+      System.out.println("  * " + document.id + " " + document.score + " :: " + document.fields.get("_id") + " :: " + document.fields.get("title"));
+    }
+
+  }
 }
 
 //public class AtlasResponse {
