6305bf284efca8d400a2f5a4f714bb1bf13e6d90
Erik Hatcher <erik.hatcher@gmail.com>
2023-08-22 18:42:42 -0400

    added query string and label, and toString formatted JSON

--- src/main/java/org/processcene/atlas/AtlasSearchRequest.java
+++ src/main/java/org/processcene/atlas/AtlasSearchRequest.java
@@ -1,18 +1,28 @@
 package org.processcene.atlas;
 
 import com.mongodb.client.model.search.SearchOperator;
+import org.bson.json.JsonWriterSettings;
 import org.processcene.core.SearchRequest;
 
 public class AtlasSearchRequest implements SearchRequest {
 
+  private final String label;
   public SearchOperator search_operator;
+  public String query_string;
 
-  public AtlasSearchRequest(SearchOperator so) {
+  public AtlasSearchRequest(String label, String query_string, SearchOperator so) {
     search_operator = so;
+    this.label = label;
+    this.query_string = query_string;
   }
 
   @Override
   public String toString() {
-    return search_operator.toString();
+    return search_operator.toBsonDocument().toJson(JsonWriterSettings.builder().indent(true).build());
+  }
+
+  @Override
+  public String getLabel() {
+    return label;
   }
 }
